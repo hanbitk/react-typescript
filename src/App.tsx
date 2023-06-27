dc735988eff48261ac2f912f8d217b848497720f
Hanbit Kang <hanbitkang95@gmail.com>
2023-06-27 18:33:55 +0900

    todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList/TodoList';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
+    }
+  };
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={(e) => handleAdd(e)} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../InputField/input.css';
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
 // const Inputfield = ({todo, setTodo}: Props) => {...}
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
         type="input"
         placeholder="Enter a Task"
         className="input__box"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
+        ref={inputRef}
       />
       <button type="submit" className="input__submit">
         Go
